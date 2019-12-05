81e1df70d4f9c010f05fa70fe861f072fef6f2b2
Tom Loomans <tom.loomans@osg.be>
2019-12-05 22:22:16 +0100

    init10

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ function requestBluetoothDevice() {
 
     log('Requesting bluetooth device...');
     return navigator.bluetooth.requestDevice({
-        filters: [{ services: ['19b10000-e8f2-537e-4f6c-d104768a1214'] }],
+        filters: [{ name: ['LED'] }],
     }).
         then(device => {
             log('"' + device.name + '" bluetooth device selected');
